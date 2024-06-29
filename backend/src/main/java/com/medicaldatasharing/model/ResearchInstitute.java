20d88d940fa76849ee5691d8ad96dcac529ad22c
buiminhhoat <official.buiminhhoat@gmail.com>
2024-06-29 22:50:17 +0700

    Add Research Institute & Scientist

---
+++ backend/src/main/java/com/medicaldatasharing/model/ResearchInstitute.java
@@ -0,0 +1,29 @@
+package com.medicaldatasharing.model;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResearchInstitute {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid")
+    @Column(columnDefinition = "CHAR(32)")
+    private String researchInstituteId;
+
+    private String name;
+
+    private String address;
+
+    private String membershipOrganizationId;
+}
---
+++ backend/src/main/java/com/medicaldatasharing/model/Scientist.java
@@ -0,0 +1,20 @@
+package com.medicaldatasharing.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+public class Scientist extends User {
+
+    @ManyToOne
+    private ResearchInstitute researchInstitute;
+}
