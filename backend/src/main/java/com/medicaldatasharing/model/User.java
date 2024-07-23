3ac3182cf7103bb84871bd6ae631d0ea8d209ead
buiminhhoat <official.buiminhhoat@gmail.com>
2024-07-23 12:24:55 +0700

    Update User

--- backend/src/main/java/com/medicaldatasharing/model/User.java
+++ backend/src/main/java/com/medicaldatasharing/model/User.java
@@ -24,6 +24,7 @@ public abstract class User implements Serializable, UserDetails {
     @GenericGenerator(name = "uuid", strategy = "uuid")
     @Column(columnDefinition = "CHAR(32)")
     private String id;
+
     @Column(name = "USER_NAME", length = 100, unique = true)
     private String username;
     private String password;
