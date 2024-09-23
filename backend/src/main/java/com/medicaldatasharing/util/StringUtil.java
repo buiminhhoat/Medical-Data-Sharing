cde5bc57fec7d16031e5022a7bc3b289d385b47e
buiminhhoat <official.buiminhhoat@gmail.com>
2024-09-23 22:42:29 +0700

    Add EncryptionUtil

--- backend/src/main/java/com/medicaldatasharing/util/StringUtil.java
+++ backend/src/main/java/com/medicaldatasharing/util/StringUtil.java
@@ -6,18 +6,6 @@ import java.util.Date;
 import java.util.Random;
 
 public class StringUtil {
-
-    public static String getIdPart(String id) {
-        return id.substring(id.lastIndexOf("/") + 1);
-    }
-
-    public static String generateMembershipOrganizationId(int orgCount) {
-        Random random = new Random();
-        int orgNum = random.nextInt(orgCount) + 1;
-
-        return "org" + orgNum;
-    }
-
     public static String parseDate(Date date) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         if (date == null) return "";
@@ -36,11 +24,5 @@ public class StringUtil {
         String searchPattern = "(?i)" + searchWord;
         return input.replaceAll(searchPattern, replacement);
     }
-
-    public static String anonymizePatientData(String input, String fullName, String lastName) {
-        input = anonymizeWord(input, fullName, "X");
-        return anonymizeWord(input, lastName, "Y");
-    }
-
 }
 
