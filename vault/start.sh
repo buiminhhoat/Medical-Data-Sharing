a6ed88a64e308f76c5d49ef04129517efbd8acac
buiminhhoat <official.buiminhhoat@gmail.com>
2024-09-25 22:39:59 +0700

    Update Vault

--- vault/start.sh
+++ vault/start.sh
@@ -1,3 +1,10 @@
+# Dừng và xóa container cũ nếu tồn tại
+docker stop my-vault-container || true
+docker rm my-vault-container || true
+
+# Xóa Docker Image nếu tồn tại
+docker rmi -f my-vault || true
+
 docker build -t my-vault .
 
 docker run -d -p 8200:8200 --name my-vault-container my-vault
